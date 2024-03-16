661a8f701f7a86529892c48ee9b2ee8b32d79b00
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2024-03-16 22:05:55 +0530

    husky, prettier setup

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import express, { Express, Request, Response } from 'express';
-import config from './config/config.js';
+import express, { Express, Request, Response } from "express";
+import config from "./config/config.js";
 const app: Express = express();
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
 });
 app.listen(config.port, () => {
   console.log(` TS Server is running on port ${config.port}`);
